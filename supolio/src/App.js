e6789135725015a9de4fb1e6cc168f80210c2c9b
dalramjwi <dalramjwi@gmail.com>
2024-09-24 18:31:55 +0900

    기본 데이터 삽입

--- supolio/src/App.js
+++ supolio/src/App.js
@@ -1,24 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <Link to="/main">main</Link>
+            <Link to="/info">info</Link>
+            <Link to="/project">project</Link>
+          </div>
+        </header>
+        <body>
+          <p>백엔드 개발자 </p>
+          <p>김수현</p>
+          <p>
+            인간이 서로 연결될 때, 기술은 단순한 도구를 넘어 의미 있는 역할을
+            합니다. 저는 그 역할을 확장해, 사람들 간의 관계를 기술로 견고하게
+            만들어주는 개발자가 되고 싶습니다.
+          </p>
+        </body>
+        <footer>
+          <Link to="/info">info</Link>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
