7d7ad64dd71a939f2e989255857d0e3c26c4c8b0
dalramjwi <dalramjwi@gmail.com>
2024-09-27 18:51:56 +0900

    main 페이지 더미 데이터 일차 구성

---
+++ supolio/src/pages/main.tsx
@@ -0,0 +1,23 @@
+// pages/main.tsx
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+const Main: React.FC = () => {
+  return (
+    <>
+      <body>
+        <p>백엔드 개발자 </p>
+        <p>김수현</p>
+        <p>
+          인간이 서로 연결될 때, 기술은 단순한 도구를 넘어 의미 있는 역할을
+          합니다. 저는 그 역할을 확장해, 사람들 간의 관계를 기술로 견고하게
+          만들어주는 개발자가 되고 싶습니다.
+        </p>
+      </body>
+      <footer>
+        <Link to="/info">info</Link>
+      </footer>
+    </>
+  );
+};
+
+export default Main;
