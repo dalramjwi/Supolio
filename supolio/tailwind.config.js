58a0d75e713b1b2973b8d1b4086dd07b270d824b
dalramjwi <dalramjwi@gmail.com>
2024-09-30 00:31:39 +0900

    theme 사용한 지정

--- supolio/tailwind.config.js
+++ supolio/tailwind.config.js
@@ -1,8 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "tab-green": "#98a88a",
+        "tab-gray": "#c7ccb5",
+        "tab-dark": "#2b3435",
+      },
+      fontSize: {
+        "heading-lg": "3.5rem",
+        "heading-md": "2rem",
+        "body-md": "1rem",
+      },
+      fontFamily: {
+        sans: ["RIDIBatang", "sans-serif"],
+      },
+      spacing: {
+        "custom-padding": "2.5rem",
+      },
+    },
   },
   plugins: [],
 };
