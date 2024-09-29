20ded0364b7c794a00097655e8b55b7eab7b3981
dalramjwi <dalramjwi@gmail.com>
2024-09-30 00:32:29 +0900

    간단한 스타일링

--- supolio/src/App.js
+++ supolio/src/App.js
@@ -1,39 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
   Navigate,
 } from "react-router-dom";
 import Main from "./pages/main.tsx";
 import Info from "./pages/info.tsx";
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
 import Project from "./pages/project.tsx";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <Link to="/main">main</Link>
-            <Link to="/info">info</Link>
-            <Link to="/project">project</Link>
-          </div>
-        </header>
-        <body>
+      <div className="App flex flex-col min-h-screen justify-between">
+        <Header />
+        <div className="flex-grow">
           <Routes>
-            {/* 기본 경로에 접속했을 때 project 페이지로 리디렉션 */}
             <Route path="/" element={<Navigate to="/project" />} />
             <Route path="/main" element={<Main />} />
             <Route path="/info" element={<Info />} />
             <Route path="/project" element={<Project />} />
           </Routes>
-        </body>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
