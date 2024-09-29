9a815c522073af077262d2b8fbc75a6ba80f7a05
dalramjwi <dalramjwi@gmail.com>
2024-09-30 00:52:22 +0900

    main.tsx 자세한 내용 작성 및 스타일링

--- supolio/src/pages/main.tsx
+++ supolio/src/pages/main.tsx
@@ -1,22 +1,21 @@
-// pages/main.tsx
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+
 const Main: React.FC = () => {
   return (
-    <>
-      <body>
-        <p>백엔드 개발자 </p>
-        <p>김수현</p>
-        <p>
+    <div className="flex flex-col min-h-full">
+      <div className="flex flex-col justify-center items-center flex-grow text-center">
+        <h1 className="text-5xl font-bold mb-6">김수현</h1>
+        <p className="text-xl mb-6">
           인간이 서로 연결될 때, 기술은 단순한 도구를 넘어 의미 있는 역할을
-          합니다. 저는 그 역할을 확장해, 사람들 간의 관계를 기술로 견고하게
-          만들어주는 개발자가 되고 싶습니다.
+          합니다.
         </p>
-      </body>
-      <footer>
-        <Link to="/info">info</Link>
-      </footer>
-    </>
+        <p className="text-xl mb-12">
+          저는 그 중 하나의 축이 되어 사람들 간의 관계를 기술로 이어주는 가교와
+          같은 개발자가 되고 싶습니다.
+        </p>
+        <span className="text-lg font-semibold">info</span>
+      </div>
+    </div>
   );
 };
 
