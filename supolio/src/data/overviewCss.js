e78931ba215fccde0f2a1215749f9fef1c6b287d
dalramjwi <dalramjwi@gmail.com>
2024-10-02 17:47:41 +0900

    overview css data 분리

---
+++ supolio/src/data/overviewCss.js
@@ -0,0 +1,11 @@
+export const projectColors = {
+  수록: "bg-yellow-400",
+  내꿈은급식왕: "bg-green-400",
+  CustomWidgets: "bg-teal-400",
+  도타임: "bg-blue-400",
+  수폴리오: "bg-purple-400",
+  수상한자판기: "bg-orange-400",
+  DelaN: "bg-pink-400",
+  갈라테이아: "bg-indigo-400",
+  호랑이: "bg-green-400",
+};
