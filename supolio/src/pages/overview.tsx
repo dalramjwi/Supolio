577eb74221ce515c1708ad4199d45925b20f42b9
dalramjwi <dalramjwi@gmail.com>
2024-10-02 17:49:13 +0900

    분리한 파일 연결

--- supolio/src/pages/overview.tsx
+++ supolio/src/pages/overview.tsx
@@ -1,32 +1,6 @@
 import React, { useState } from "react";
-
-const workPeriod = {
-  personal: {
-    수록: { start: "5.24", end: "6.12" },
-    내꿈은급식왕: { start: "5.8", end: "9.11" },
-    CustomWidgets: { start: "7.17", end: "9.18" },
-    도타임: { start: "9.17", end: "NaN" },
-    수폴리오: { start: "9.20", end: "10.3" },
-  },
-  team: {
-    수상한자판기: { start: "6.20", end: "7.15" },
-    DelaN: { start: "7.16", end: "10.11" },
-    갈라테이아: { start: "7.24", end: "NaN" },
-    호랑이: { start: "9.12", end: "10.10" },
-  },
-};
-
-const projectColors: { [key: string]: string } = {
-  수록: "bg-yellow-400",
-  내꿈은급식왕: "bg-green-400",
-  CustomWidgets: "bg-teal-400",
-  도타임: "bg-blue-400",
-  수폴리오: "bg-purple-400",
-  수상한자판기: "bg-orange-400",
-  DelaN: "bg-pink-400",
-  갈라테이아: "bg-indigo-400",
-  호랑이: "bg-green-400",
-};
+import { workPeriod } from "../data/overview";
+import { projectColors } from "../data/overviewCss";
 
 const parseDate = (dateStr: string, year: number) => {
   const [month, day] = dateStr.split(".").map(Number);
