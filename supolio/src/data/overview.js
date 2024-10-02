3f425d2bae2739dc37adb6e3cb856a46a6e0d8f5
dalramjwi <dalramjwi@gmail.com>
2024-10-02 17:47:32 +0900

    overview data 분리

---
+++ supolio/src/data/overview.js
@@ -0,0 +1,15 @@
+export const workPeriod = {
+  personal: {
+    수록: { start: "5.24", end: "6.12" },
+    내꿈은급식왕: { start: "5.8", end: "9.11" },
+    CustomWidgets: { start: "7.17", end: "9.18" },
+    도타임: { start: "9.17", end: "NaN" },
+    수폴리오: { start: "9.20", end: "10.3" },
+  },
+  team: {
+    수상한자판기: { start: "6.20", end: "7.15" },
+    DelaN: { start: "7.16", end: "10.11" },
+    갈라테이아: { start: "7.24", end: "NaN" },
+    호랑이: { start: "9.12", end: "10.10" },
+  },
+};
