2d184c69e1051b8f54d3cfff61e6cb7a4abb1b83
dalramjwi <dalramjwi@gmail.com>
2024-10-03 17:40:59 +0900

    프로젝트 리스트 함수 분리

---
+++ supolio/src/components/ProjectList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { projectColors } from "../data/overviewCss";
+
+interface Props {
+  handleProjectClick: (project: string) => void;
+}
+
+// 프로젝트 리스트를 렌더링하는 컴포넌트
+const ProjectList: React.FC<Props> = ({ handleProjectClick }) => {
+  return (
+    <div className="flex flex-col justify-center gap-4 pl-8">
+      {Object.keys(projectColors).map((project) => (
+        <div
+          key={project}
+          className={`p-3 text-black rounded flex items-center justify-between cursor-pointer ${projectColors[project]}`}
+          onClick={() => handleProjectClick(project)}
+        >
+          {project}
+          <span>▶</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProjectList;
