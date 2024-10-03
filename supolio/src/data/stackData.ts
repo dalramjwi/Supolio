b7f856ce731bd1a0a7efb9df00801608dbfa793c
dalramjwi <dalramjwi@gmail.com>
2024-10-03 20:40:41 +0900

    stack data 분리

---
+++ supolio/src/data/stackData.ts
@@ -0,0 +1,39 @@
+export const stackData = [
+  {
+    title: "Language",
+    items: ["JavaScript", "TypeScript", "Python"],
+    colorClass: "bg-yellow-100 border-yellow-300",
+  },
+  {
+    title: "Framework / Library",
+    items: [
+      "React",
+      "Next.js",
+      "Nest.js",
+      "Node.js",
+      "TailwindCSS",
+      "Vanilla Extract",
+    ],
+    colorClass: "bg-blue-100 border-blue-300",
+  },
+  {
+    title: "MarkUp",
+    items: ["HTML5", "CSS3"],
+    colorClass: "bg-green-100 border-green-300",
+  },
+  {
+    title: "Database",
+    items: ["Sqlite3", "PostgreSQL", "MongoDB"],
+    colorClass: "bg-gray-100 border-gray-300",
+  },
+  {
+    title: "Tool",
+    items: ["Git", "GitHub", "Figma", "Notion"],
+    colorClass: "bg-purple-100 border-purple-300",
+  },
+  {
+    title: "AWS",
+    items: ["S3", "Amplify", "EC2"],
+    colorClass: "bg-orange-100 border-orange-300",
+  },
+];
