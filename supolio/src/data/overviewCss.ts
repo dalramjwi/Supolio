4e3dfe72771342024a69153cb178559efa13e725
dalramjwi <dalramjwi@gmail.com>
2024-10-05 16:57:06 +0900

    도타임 주석처리

--- supolio/src/data/overviewCss.ts
+++ supolio/src/data/overviewCss.ts
@@ -3,8 +3,9 @@ export const projectColors = {
   수록: "bg-[#B7C98B]", // Soft Mint Green
   내꿈은급식왕: "bg-[#A1D3E0]", // Light Sky Blue
   CustomWidgets: "bg-[#7AB9E2]", // Brighter Steel Blue
-  도타임: "bg-[#B0D7A4]", // Light Salmon
-  수폴리오: "bg-[#86C7D6]", // Light Aqua
+  // 도타임: "bg-[#B0D7A4]", // Light Salmon
+  수폴리오: "bg-[#B0D7A4]", // Light Aqua
+  // 수폴리오: "bg-[#86C7D6]", // Light Aqua
 
   수상한자판기: "bg-[#E9E4D4]", // Warm Light Brown
   DelaN: "bg-[#D2C9B4]", // Soft Beige
