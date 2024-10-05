135303fd4aec4c32caf8f9f83a3934e8739aa0c0
dalramjwi <dalramjwi@gmail.com>
2024-10-05 18:17:57 +0900

    overview 페이지 css 통일

--- supolio/src/pages/Overview.tsx
+++ supolio/src/pages/Overview.tsx
@@ -40,95 +40,103 @@ const Overview: React.FC = () => {
   const weeks: number[][] = generateCalendar(currentYear, currentMonth);
 
   return (
-    <div className="flex justify-center items-end">
-      <div className="pt-10 pr-10 pb-0 pl-10 text-center">
-        <h2 className="text-5xl font-bold mb-2">{months[currentMonth - 4]}</h2>
+    <div className="flex flex-col min-h-full bg-gray-100 p-6 ">
+      <div className="flex flex-col justify-center items-center flex-grow text-center bg-white rounded-lg shadow-lg border border-gray-300">
+        <div className="flex justify-center items-end">
+          <div className="pr-10 pb-0 text-center">
+            <h2 className="text-5xl font-bold mb-2">
+              {months[currentMonth - 4]}
+            </h2>
 
-        {/* 이전/다음 달 버튼 */}
-        <MonthNavigation
-          currentMonth={currentMonth}
-          handlePrevMonth={handlePrevMonth}
-          handleNextMonth={handleNextMonth}
-        />
+            {/* 이전/다음 달 버튼 */}
+            <MonthNavigation
+              currentMonth={currentMonth}
+              handlePrevMonth={handlePrevMonth}
+              handleNextMonth={handleNextMonth}
+            />
 
-        <div className="flex flex-row gap-10">
-          {/* 프로젝트 리스트 */}
-          <div className="left-80 z-10">
-            <ProjectList handleProjectClick={handleProjectClick} />
-          </div>
+            <div className="flex flex-row gap-10">
+              {/* 프로젝트 리스트 */}
+              <div className="left-80 z-10">
+                <ProjectList handleProjectClick={handleProjectClick} />
+              </div>
 
-          {/* 달력 */}
-          <table className="table-fixed w-full max-w-3xl mx-auto border-collapse border border-gray-400">
-            <thead>
-              <tr>
-                {["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"].map(
-                  (day) => (
-                    <th
-                      key={day}
-                      className="w-1/7 border border-gray-400 py-4 text-xl"
-                    >
-                      {day}
-                    </th>
-                  )
-                )}
-              </tr>
-            </thead>
-            <tbody>
-              {weeks.map((week, i) => (
-                <tr key={i}>
-                  {week.map((day, j) => (
-                    <td
-                      key={j}
-                      className="relative border border-gray-400 text-xl text-right align-top pr-2 h-24"
-                    >
-                      {!isNaN(day) && <div className="z-10 pt-1">{day}</div>}
+              {/* 달력 */}
+              <table className="table-fixed w-full max-w-3xl mx-auto border-collapse border border-gray-400">
+                <thead>
+                  <tr>
+                    {["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"].map(
+                      (day) => (
+                        <th
+                          key={day}
+                          className="w-1/7 border border-gray-400 py-4 text-xl"
+                        >
+                          {day}
+                        </th>
+                      )
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {weeks.map((week, i) => (
+                    <tr key={i}>
+                      {week.map((day, j) => (
+                        <td
+                          key={j}
+                          className="relative border border-gray-400 text-xl text-right align-top pr-2 h-24"
+                        >
+                          {!isNaN(day) && (
+                            <div className="z-10 pt-1">{day}</div>
+                          )}
 
-                      {/* 선택된 프로젝트가 있을 때 해당 프로젝트 표시 */}
-                      {selectedProject &&
-                        getProjectInDay(
-                          day,
-                          currentMonth,
-                          currentYear,
-                          workPeriod.personal
-                        )
-                          .filter((project) => project === selectedProject)
-                          .map((project) => (
-                            <div
-                              key={project}
-                              className={`${projectColors[project]} absolute w-full h-6 left-0`}
-                              title={project}
-                              style={{
-                                top: "60%",
-                                transform: "translateY(-50%)",
-                              }}
-                            ></div>
-                          ))}
+                          {/* 선택된 프로젝트가 있을 때 해당 프로젝트 표시 */}
+                          {selectedProject &&
+                            getProjectInDay(
+                              day,
+                              currentMonth,
+                              currentYear,
+                              workPeriod.personal
+                            )
+                              .filter((project) => project === selectedProject)
+                              .map((project) => (
+                                <div
+                                  key={project}
+                                  className={`${projectColors[project]} absolute w-full h-6 left-0`}
+                                  title={project}
+                                  style={{
+                                    top: "60%",
+                                    transform: "translateY(-50%)",
+                                  }}
+                                ></div>
+                              ))}
 
-                      {selectedProject &&
-                        getProjectInDay(
-                          day,
-                          currentMonth,
-                          currentYear,
-                          workPeriod.team
-                        )
-                          .filter((project) => project === selectedProject)
-                          .map((project) => (
-                            <div
-                              key={project}
-                              className={`${projectColors[project]} absolute w-full h-6 left-0`}
-                              title={project}
-                              style={{
-                                top: "60%",
-                                transform: "translateY(-50%)",
-                              }}
-                            ></div>
-                          ))}
-                    </td>
+                          {selectedProject &&
+                            getProjectInDay(
+                              day,
+                              currentMonth,
+                              currentYear,
+                              workPeriod.team
+                            )
+                              .filter((project) => project === selectedProject)
+                              .map((project) => (
+                                <div
+                                  key={project}
+                                  className={`${projectColors[project]} absolute w-full h-6 left-0`}
+                                  title={project}
+                                  style={{
+                                    top: "60%",
+                                    transform: "translateY(-50%)",
+                                  }}
+                                ></div>
+                              ))}
+                        </td>
+                      ))}
+                    </tr>
                   ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
