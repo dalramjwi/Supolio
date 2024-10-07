2a30b0effeffa19db37826de0824e1fc7a0375ad
dalramjwi <dalramjwi@gmail.com>
2024-10-07 10:22:43 +0900

    프로젝트 색상 투명도 조절

--- supolio/src/data/overviewCss.ts
+++ supolio/src/data/overviewCss.ts
@@ -1,10 +1,10 @@
 // 프로젝트별로 배정된 배경색을 나타내는 객체
 export const projectColors = {
-  수록: "bg-[#B7C98B]", // Soft Mint Green
-  내꿈은급식왕: "bg-[#A1D3E0]", // Light Sky Blue
-  CustomWidgets: "bg-[#7AB9E2]", // Brighter Steel Blue
+  수록: "bg-[#B7C98BAA]", // Soft Mint Green
+  내꿈은급식왕: "bg-[#A1D3E0AA]", // Light Sky Blue
+  CustomWidgets: "bg-[#7AB9E2AA]", // Brighter Steel Blue
   // 도타임: "bg-[#B0D7A4]", // Light Salmon
-  수폴리오: "bg-[#B0D7A4]", // Light Aqua
+  수폴리오: "bg-[#B0D7A4AA]", // Light Aqua
   // 수폴리오: "bg-[#86C7D6]", // Light Aqua
 
   수상한자판기: "bg-[#E9E4D4]", // Warm Light Brown
