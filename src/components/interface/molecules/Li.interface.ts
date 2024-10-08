1a72ceb73a3d894c08ed7c28e50ae6eb9ba15a2c
dalramjwi <dalramjwi@gmail.com>
2024-10-08 16:46:03 +0900

    li interface 파일 추가

---
+++ src/components/interface/molecules/Li.interface.ts
@@ -0,0 +1,5 @@
+import { LiHTMLAttributes } from "react";
+import { CssProps } from "../atoms/CssProps.interface";
+
+// HTML li 요소의 기본 속성을 확장한 인터페이스
+export interface LiProps extends LiHTMLAttributes<HTMLLIElement>, CssProps {}
