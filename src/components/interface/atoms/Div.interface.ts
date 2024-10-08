04a0dd8aa836f489e729b9e81ad7678498c29911
dalramjwi <dalramjwi@gmail.com>
2024-10-08 15:52:46 +0900

    typescript html 기본 요소를 확장한 interface 제작

--- src/components/interface/atoms/Div.interface.ts
+++ src/components/interface/atoms/Div.interface.ts
@@ -1,4 +1,6 @@
-export interface DivProps {
-  children: React.ReactNode;
+import React, { HTMLAttributes } from "react";
+
+// HTML div 요소의 기본 속성을 확장한 interface
+export interface DivProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
