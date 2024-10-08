6335f0a4ce23635c7c45f054ac430a9bcf1b22a9
dalramjwi <dalramjwi@gmail.com>
2024-10-08 16:48:07 +0900

    경로 수정

--- src/components/interface/molecules/Anchor.interface.ts
+++ src/components/interface/molecules/Anchor.interface.ts
@@ -1,5 +1,5 @@
 import { AnchorHTMLAttributes } from "react";
-import { CssProps } from "./CssProps.interface";
+import { CssProps } from "../atoms/CssProps.interface";
 
 // HTML a 요소의 기본 속성을 확장한 인터페이스
 export interface AnchorProps
