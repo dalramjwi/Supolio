0fdbde170616b656ac2e1a3853a5b4bbd3e263c9
dalramjwi <dalramjwi@gmail.com>
2024-10-08 17:28:02 +0900

    NavLink의 조합 필요치 않음, 파일 삭제

--- src/components/interfaces/molecules/NavLinkProps.interface.ts
+++
@@ -1,5 +0,0 @@
-import { NavLinkProps as ReactNavLinkProps } from "react-router-dom";
-import { CssProps } from "../atoms/CssProps.interface";
-
-// NavLink의 기본 속성을 확장한 인터페이스
-export interface NavLinkProps extends ReactNavLinkProps, CssProps {}
