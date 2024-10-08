013f596bb0230104c1483c453c707290717cac20
dalramjwi <dalramjwi@gmail.com>
2024-10-08 17:10:24 +0900

    Div 수정 - 경로

--- src/components/atoms/Div.tsx
+++ src/components/atoms/Div.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DivProps } from "../interface/molecules/Div.interface";
+import { DivProps } from "../interfaces/molecules/Div.interface";
 
 const Div: React.FC<DivProps> = ({ children, className }) => {
   return <div className={className}>{children}</div>;
