d93ceb06ab7742d4325e07d4001650c3ba5f2ec1
dalramjwi <dalramjwi@gmail.com>
2024-10-08 16:47:30 +0900

    a tag interface 정리

---
+++ src/components/interface/molecules/Anchor.interface.ts
@@ -0,0 +1,7 @@
+import { AnchorHTMLAttributes } from "react";
+import { CssProps } from "./CssProps.interface";
+
+// HTML a 요소의 기본 속성을 확장한 인터페이스
+export interface AnchorProps
+  extends AnchorHTMLAttributes<HTMLAnchorElement>,
+    CssProps {}
