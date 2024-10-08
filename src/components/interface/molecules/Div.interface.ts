f2e1dc6ce59c0bc7ad66f2081a1dcb16eae6dd06
dalramjwi <dalramjwi@gmail.com>
2024-10-08 16:18:18 +0900

    children 속성 제외 후 수정

--- src/components/interface/molecules/Div.interface.ts
+++ src/components/interface/molecules/Div.interface.ts
@@ -1,9 +1,5 @@
 import { HTMLAttributes } from "react";
 import { CssProps } from "../atoms/CssProps.interface";
-import { ChildrenProps } from "../atoms/ChildrenProps.interface";
 
 // HTML div 요소의 기본 속성을 확장한 인터페이스
-export interface DivProps
-  extends HTMLAttributes<HTMLDivElement>,
-    CssProps,
-    ChildrenProps {}
+export interface DivProps extends HTMLAttributes<HTMLDivElement>, CssProps {}
