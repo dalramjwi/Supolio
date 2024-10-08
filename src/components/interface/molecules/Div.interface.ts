88eafaf8673192d4c9eee4d6e4a77c59d14f5af4
dalramjwi <dalramjwi@gmail.com>
2024-10-08 16:17:54 +0900

    children props와 html attributes props - children 중복으로 오류 발생

--- src/components/interface/molecules/Div.interface.ts
+++ src/components/interface/molecules/Div.interface.ts
@@ -1,5 +1,9 @@
 import { HTMLAttributes } from "react";
 import { CssProps } from "../atoms/CssProps.interface";
+import { ChildrenProps } from "../atoms/ChildrenProps.interface";
 
 // HTML div 요소의 기본 속성을 확장한 인터페이스
-export interface DivProps extends HTMLAttributes<HTMLDivElement>, CssProps {}
+export interface DivProps
+  extends HTMLAttributes<HTMLDivElement>,
+    CssProps,
+    ChildrenProps {}
