f10a1f9bd3c7e2d2d94aa0c039361d7c2902302b
dalramjwi <dalramjwi@gmail.com>
2024-10-08 17:08:27 +0900

    Img interface 작성

---
+++ src/components/interfaces/molecules/Img.interface.ts
@@ -0,0 +1,9 @@
+import { ImgHTMLAttributes } from "react";
+import { CssProps } from "../atoms/CssProps.interface";
+// HTML img 요소의 기본 속성을 확장한 인터페이스
+export interface ImgProps
+  extends ImgHTMLAttributes<HTMLImageElement>,
+    CssProps {
+  src: string;
+  alt: string;
+}
