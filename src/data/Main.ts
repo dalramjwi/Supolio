29b3d0f249e0d3738937cda656b12d8a36e73ed6
dalramjwi <dalramjwi@gmail.com>
2024-10-18 18:10:41 +0900

    Main.ts
    main.tsx에서 사용할 데이터 class 제작

---
+++ src/data/Main.ts
@@ -0,0 +1,10 @@
+export class MainData {
+  name: string;
+  paragraph: string;
+  description: string;
+  constructor(name: string, paragraph: string, description: string) {
+    this.name = name;
+    this.paragraph = paragraph;
+    this.description = description;
+  }
+}
