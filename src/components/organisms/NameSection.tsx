8bdba9a1c4c0f63facebb1d23b970e55ba180bf6
dalramjwi <dalramjwi@gmail.com>
2024-10-18 17:36:28 +0900

    NameSection.tsx
    organsims - 기능 단위로 분해하기 위해, 이름을 나타내는 div 및 style 적용해서 생성

---
+++ src/components/organisms/NameSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Div from "../atoms/Div.tsx";
+const NameSection: React.FC = () => {
+  return (
+    <Div className="text-6xl font-extrabold text-gray-800 mb-8 relative z-10">
+      김수현
+    </Div>
+  );
+};
+
+export default NameSection;
