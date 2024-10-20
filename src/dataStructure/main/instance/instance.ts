b34c8e2eaf4c46234bb2a4c29ccdeff298a01a2a
dalramjwi <dalramjwi@gmail.com>
2024-10-20 22:29:02 +0900

    todo 삭제

--- src/dataStructure/main/instance/instance.ts
+++ src/dataStructure/main/instance/instance.ts
@@ -1,4 +1,3 @@
 import { MainDataStructure } from "../class/class.ts";
 import dataJson from "../../../data/data.json";
-//todo 데이터 json으로 빼기
 export const MainInstance = MainDataStructure.fromJson(dataJson);
