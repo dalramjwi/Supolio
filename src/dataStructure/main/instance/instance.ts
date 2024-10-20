f9c9d1394dd35615235c1b5f2c4af201a4ceca05
dalramjwi <dalramjwi@gmail.com>
2024-10-20 22:24:06 +0900

    instnace에서 사용하는 리터럴 값 data.json에서 불러와서 사용

--- src/dataStructure/main/instance/instance.ts
+++ src/dataStructure/main/instance/instance.ts
@@ -1,7 +1,4 @@
 import { MainDataStructure } from "../class/class.ts";
+import dataJson from "../../../data/data.json";
 //todo 데이터 json으로 빼기
-export const MainInstance = new MainDataStructure(
-  "김수현",
-  "About Me",
-  "인간이 서로 연결될 때 기술은 단순한 도구를 넘어 더욱 의미 있는 역할을 하게 됩니다."
-);
+export const MainInstance = MainDataStructure.fromJson(dataJson);
