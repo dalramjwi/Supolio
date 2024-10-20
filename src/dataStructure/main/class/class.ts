b4d7c788655854f77454d65a44fc0f054a5cf724
dalramjwi <dalramjwi@gmail.com>
2024-10-20 22:22:20 +0900

    class 이름 수정 maindatastructure 및 private, public , fromJson 제작

---
+++ src/dataStructure/main/class/class.ts
@@ -0,0 +1,21 @@
+export class MainDataStructure {
+  // public 필드들은 외부에서 접근 가능
+  public name: string;
+  public paragraph: string;
+  public description: string;
+
+  // private 생성자, 외부에서 직접 인스턴스를 만들 수 없도록 함
+  private constructor(name: string, paragraph: string, description: string) {
+    this.name = name;
+    this.paragraph = paragraph;
+    this.description = description;
+  }
+
+  // JSON 데이터를 받아 인스턴스를 생성하는 정적 메서드
+  public static fromJson(json: {
+    main: { name: string; paragraph: string; description: string };
+  }): MainDataStructure {
+    const { name, paragraph, description } = json.main;
+    return new MainDataStructure(name, paragraph, description);
+  }
+}
