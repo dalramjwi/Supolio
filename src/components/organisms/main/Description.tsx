6e339d254d3d77e7ca955622f914be8adc9215f3
dalramjwi <dalramjwi@gmail.com>
2024-10-20 23:13:20 +0900

    description htmlelementdiv 사용

--- src/components/organisms/main/Description.tsx
+++ src/components/organisms/main/Description.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Div from "../../atoms/Div.tsx";
+import HtmlElementDiv from "./../../molecules/HtmlElementDiv.tsx";
 import { MainInstance } from "../../../dataStructure/main/instance/instance.ts";
 //todo css 빼기
 const DescriptionSection: React.FC = () => {
   return (
-    <Div className="text-xl text-gray-600 mb-4 mt-3 relative z-10">
-      {MainInstance.description}
-    </Div>
+    <HtmlElementDiv
+      className="text-xl text-gray-600 mb-4 mt-3 relative z-10"
+      htmlContent={MainInstance.description}
+    ></HtmlElementDiv>
   );
 };
 
