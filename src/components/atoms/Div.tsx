0ffc718ae1b17cb9a74cd9814a487ca22aaa5c5d
dalramjwi <dalramjwi@gmail.com>
2024-10-21 19:22:49 +0900

    modal 닫히지 않는 문제 해결 : div 속성 전달

--- src/components/atoms/Div.tsx
+++ src/components/atoms/Div.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { DivProps } from "../interfaces/molecules/Div.interface";
 
-const Div: React.FC<DivProps> = ({ children, className }) => {
-  return <div className={className}>{children}</div>;
+const Div: React.FC<DivProps> = ({ children, className, ...rest }) => {
+  return (
+    <div className={className} {...rest}>
+      {children}
+    </div>
+  );
 };
 
 export default Div;
