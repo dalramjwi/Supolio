29bb14612d0552e4f94dba6308df4eb6f3ec9594
dalramjwi <dalramjwi@gmail.com>
2024-10-21 17:47:47 +0900

    modal.tsx modalprops 사용 및 todo 추가

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -1,27 +1,22 @@
 import React from "react";
-
-interface ModalProps {
-  show: boolean;
-  close: () => void;
-  content: React.ReactNode;
-}
-
-//모달 컴포넌트
+import Div from "../atoms/Div.tsx";
+import { ModalProps } from "../interfaces/molecules/Modal.interface.ts";
+//todo css 빼기 및 css 전달 필요
 const Modal: React.FC<ModalProps> = ({ show, close, content }) => {
   if (!show) return null;
 
   return (
-    <div
+    <Div
       className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
       onClick={close} // 모달 외부를 클릭하면 닫힘
     >
-      <div
+      <Div
         className="bg-white text-black p-6 rounded-lg shadow-lg"
         onClick={(e) => e.stopPropagation()} // 모달 내부를 클릭해도 닫히지 않게 처리
       >
         {content}
-      </div>
-    </div>
+      </Div>
+    </Div>
   );
 };
 
