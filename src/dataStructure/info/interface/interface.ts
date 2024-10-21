7a3e9c6c3c1bb78268ef315b19f1e3189da72bfd
dalramjwi <dalramjwi@gmail.com>
2024-10-21 13:04:34 +0900

    interface의 stack, study 구체화

--- src/dataStructure/info/interface/interface.ts
+++ src/dataStructure/info/interface/interface.ts
@@ -2,6 +2,17 @@ export interface InfoData {
   phoneNumber: string;
   email: string;
   github: string;
-  stack: object;
-  study: object;
+  stack: {
+    language: string[];
+    framework: string[];
+    makeup: string[];
+    database: string[];
+    tool: string[];
+    devops: string[];
+  };
+  study: {
+    title: string[];
+    description: string[];
+    imgurl: string[];
+  };
 }
