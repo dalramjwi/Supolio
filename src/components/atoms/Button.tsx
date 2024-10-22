a928286589095ab64a89151940524f0a150a0c30
dalramjwi <dalramjwi@gmail.com>
2024-10-22 23:20:32 +0900

    button에 나머지 속성 적용

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { ButtonProps } from "../interfaces/molecules/Button.interface";
 
-const Button: React.FC<ButtonProps> = ({ children, className, onClick }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  onClick,
+  ...rest
+}) => {
   return (
-    <button className={className} onClick={onClick}>
+    <button className={className} onClick={onClick} {...rest}>
       {children}
     </button>
   );
