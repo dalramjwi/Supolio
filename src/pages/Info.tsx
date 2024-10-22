b09ec3d004d7d935936705df74e37b4414a51eb6
dalramjwi <dalramjwi@gmail.com>
2024-10-22 22:34:20 +0900

    info에서 modal 관리 삭제

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,29 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import data from "../data/data.json";
-import StudyModal from "../components/StudyModal.tsx";
 import InfoList from "../components/organisms/info/InfoList.tsx";
 
 const Info: React.FC = () => {
-  const [showStudyModal, setShowStudyModal] = useState(false);
-
-  const handleAboutStudyClick = () => {
-    setShowStudyModal(true);
-  };
-
   const infoData = data.info;
+
   //todo css 변경 필요
   return (
     <div className="flex flex-col min-h-full bg-gray-100 p-6">
       <div className="flex flex-col justify-center items-center flex-grow text-center bg-white rounded-lg shadow-lg p-10 border border-gray-300">
-        {/* InfoList 컴포넌트에 데이터와 클릭 핸들러 전달 */}
-        <InfoList infoData={infoData} onStudyClick={handleAboutStudyClick} />
+        {/* InfoList 컴포넌트에 데이터 전달 */}
+        <InfoList infoData={infoData} />
       </div>
-
-      {/* Study Modal */}
-      <StudyModal
-        show={showStudyModal}
-        close={() => setShowStudyModal(false)}
-      />
     </div>
   );
 };
