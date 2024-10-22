e3e382602851b96c854102eaf78ad612ebf9c803
dalramjwi <dalramjwi@gmail.com>
2024-10-22 18:35:54 +0900

    Info List 연결

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
-
 import data from "../data/data.json"; // 데이터 불러오기
-import Div from "../components/atoms/Div.tsx";
-import Img from "../components/atoms/Img.tsx";
-import Anchor from "../components/atoms/Anchor.tsx";
 import StudyModal from "../components/StudyModal.tsx";
+import InfoList from "../components/organisms/info/InfoList.tsx";
 
 const Info: React.FC = () => {
   const [showStudyModal, setShowStudyModal] = useState(false);
@@ -18,54 +15,8 @@ const Info: React.FC = () => {
   return (
     <div className="flex flex-col min-h-full bg-gray-100 p-6">
       <div className="flex flex-col justify-center items-center flex-grow text-center bg-white rounded-lg shadow-lg p-10 border border-gray-300">
-        <div className="grid grid-cols-1 md:grid-cols-3 text-center gap-8 mb-4 w-full">
-          {Object.keys(infoData).map((key) => {
-            const { data, method } = infoData[key];
-            return (
-              <Div key={key} className="pb-4 mb-4 w-full">
-                <h3 className="text-2xl font-bold mb-2 border-b-2 border-gray-400 pb-2">
-                  {key.toUpperCase()}
-                </h3>
-                <Div className="content">
-                  {(() => {
-                    switch (method) {
-                      case "img":
-                        return (
-                          <Img
-                            src={data}
-                            alt={key}
-                            className="h-[15vh] top-12"
-                          />
-                        );
-                      case "a":
-                        return (
-                          <Anchor
-                            href={data}
-                            className="text-blue-700 underline text-xl"
-                          >
-                            {key}
-                          </Anchor>
-                        );
-                      case "text":
-                        return <p>{JSON.stringify(data)}</p>;
-                      case "modal":
-                        return (
-                          <p
-                            onClick={handleAboutStudyClick}
-                            className="cursor-pointer text-blue-700 underline text-xl"
-                          >
-                            ABOUT {key.toUpperCase()}
-                          </p>
-                        );
-                      default:
-                        return null;
-                    }
-                  })()}
-                </Div>
-              </Div>
-            );
-          })}
-        </div>
+        {/* InfoList 컴포넌트에 데이터와 클릭 핸들러 전달 */}
+        <InfoList infoData={infoData} onStudyClick={handleAboutStudyClick} />
       </div>
 
       {/* Study Modal */}
