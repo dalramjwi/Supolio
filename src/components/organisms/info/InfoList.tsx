862c8ccddc4322cccb050915e6b1e30f0e956009
dalramjwi <dalramjwi@gmail.com>
2024-10-22 18:35:41 +0900

    InfoList 임시 제작

---
+++ src/components/organisms/info/InfoList.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Div from "../../atoms/Div.tsx";
+import Img from "../../atoms/Img.tsx";
+import Anchor from "../../atoms/Anchor.tsx";
+
+interface InfoListProps {
+  infoData: { [key: string]: { data: string; method: string } };
+  onStudyClick: () => void;
+}
+
+const InfoList: React.FC<InfoListProps> = ({ infoData, onStudyClick }) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 text-center gap-8 mb-4 w-full">
+      {Object.keys(infoData).map((key) => {
+        const { data, method } = infoData[key];
+        return (
+          <Div key={key} className="pb-4 mb-4 w-full">
+            <Div className="text-2xl font-bold mb-2 border-b-2 border-gray-400 pb-2">
+              {key.toUpperCase()}
+            </Div>
+            <Div className="content">
+              {(() => {
+                switch (method) {
+                  case "img":
+                    return (
+                      <Img src={data} alt={key} className="h-[15vh] top-12" />
+                    );
+                  case "a":
+                    return (
+                      <Anchor
+                        href={data}
+                        className="text-blue-700 underline text-xl"
+                      >
+                        {key}
+                      </Anchor>
+                    );
+                  case "text":
+                    return <p>{JSON.stringify(data)}</p>;
+                  case "modal":
+                    return (
+                      <p
+                        onClick={onStudyClick}
+                        className="cursor-pointer text-blue-700 underline text-xl"
+                      >
+                        ABOUT {key.toUpperCase()}
+                      </p>
+                    );
+                  default:
+                    return null;
+                }
+              })()}
+            </Div>
+          </Div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default InfoList;
