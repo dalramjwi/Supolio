f23977ccc5ae017519e11733ce0f2c3bae126172
dalramjwi <dalramjwi@gmail.com>
2024-10-23 11:40:21 +0900

    InfoTemplate, Info와 같은 환경 조성 : 추후에 template은 css와 매핑되어야 함

--- src/components/templates/InfoTemplate.tsx
+++ src/components/templates/InfoTemplate.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import InfoList from "../organisms/info/InfoList";
+import data from "../../data/data.json";
 
 const InfoTemplate: React.FC = () => {
+  const infoData = data.info;
   return (
     <div className="flex flex-col min-h-full bg-gray-100 p-6">
-      {/* Info 내용 추가 */}
-      <p>Info 페이지 템플릿입니다.</p>
+      <div className="flex flex-col justify-center items-center flex-grow text-center bg-white rounded-lg shadow-lg p-10 border border-gray-300">
+        {/* InfoList 컴포넌트에 데이터 전달 */}
+        <InfoList infoData={infoData} />
+      </div>
     </div>
   );
 };
