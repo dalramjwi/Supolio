0bf406f02c4ca80a750dc82d2a230c0fc1fdce50
dalramjwi <dalramjwi@gmail.com>
2024-10-23 20:24:24 +0900

    json data 직접적으로 사용하는 것이 아닌, instance 사용으로 변경

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import data from "../data/data.json";
 import InfoList from "../components/organisms/info/InfoList.tsx";
+import { InfoInstance } from "../dataStructure/info/instance/instance.ts";
 
 const Info: React.FC = () => {
-  const infoData = data.info;
-
   //todo css 변경 필요
   return (
     <div className="flex flex-col min-h-full bg-gray-100 p-6">
       <div className="flex flex-col justify-center items-center flex-grow text-center bg-white rounded-lg shadow-lg p-10 border border-gray-300">
         {/* InfoList 컴포넌트에 데이터 전달 */}
-        <InfoList infoData={infoData} />
+        <InfoList infoData={InfoInstance} />
       </div>
     </div>
   );
