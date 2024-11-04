ef49ed7f44fb9bae569e8dee081c1fa94799b6ac
dalramjwi <dalramjwi@gmail.com>
2024-11-04 22:16:38 +0900

    content json 추가 제작

--- src/dataStructure/project/interface/projectJson.ts
+++ src/dataStructure/project/interface/projectJson.ts
@@ -1,8 +1,12 @@
-import { ProjectData } from "./projectData.ts";
+import { ProjectData, TeamProjectData } from "./projectData.ts";
 
 export interface ProjectJson {
-    project: {
-        single: ProjectData[];
-        team: ProjectData[];
-    };
+  project: {
+    single: ProjectData[];
+    team: TeamProjectData[];
+  };
+}
+export interface ProjectContentJson {
+  single: ProjectData[];
+  team: TeamProjectData[];
 }
