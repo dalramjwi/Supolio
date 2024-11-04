fa5c4b1a211d34d44c9c1ab749be1363fb79aad0
dalramjwi <dalramjwi@gmail.com>
2024-11-04 22:15:49 +0900

    teamprojectdata interface 추가

--- src/dataStructure/project/interface/projectData.ts
+++ src/dataStructure/project/interface/projectData.ts
@@ -1,13 +1,25 @@
 export interface ProjectData {
-    name: string;
-    description: string;
-    introduce: string;
-    detail: string;
-    contribute?: string;
-    img: { description: string; imgurl: string }[];
-    review: string;
-    period: string[];
-    stack: string[];
-    link: (string | string[])[];
-    thumbnail: string;
+  name: string;
+  description: string;
+  introduce: string;
+  detail: string;
+  img: { description: string; imgurl: string }[];
+  review: string;
+  period: string[];
+  stack: string[];
+  link: (string | string[])[];
+  thumbnail: string;
+}
+export interface TeamProjectData {
+  name: string;
+  description: string;
+  introduce: string;
+  detail: string;
+  contribute: string;
+  img: { description: string; imgurl: string }[];
+  review: string;
+  period: string[];
+  stack: string[];
+  link: (string | string[])[];
+  thumbnail: string;
 }
