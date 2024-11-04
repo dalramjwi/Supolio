fd6c5921edae07e9be593e2ba32df19cc5de2ac5
dalramjwi <dalramjwi@gmail.com>
2024-11-04 22:14:09 +0900

    json key 유틸리티 함수 사용 : infodata

--- src/dataStructure/info/class/infoDataStructure.ts
+++ src/dataStructure/info/class/infoDataStructure.ts
@@ -1,42 +1,36 @@
+import { fromJsonByKey } from "../../json/fromJsonByKey.ts";
 import { InfoData } from "../interface/infoData.ts";
 import { InfoJson } from "../interface/infoJson.ts";
 
 export class InfoDataStructure {
-    public phoneNumber: { data: string; method: string };
-    public email: { data: string; method: string };
-    public github: { data: string; method: string; paragraph: string };
-    public stack: {
-        data: {
-            language: string[];
-            framework: string[];
-            markup: string[];
-            database: string[];
-            tool: string[];
-            devops: string[];
-        };
-        method: string;
+  public phoneNumber: { data: string; method: string };
+  public email: { data: string; method: string };
+  public github: { data: string; method: string; paragraph: string };
+  public stack: {
+    data: {
+      language: string[];
+      framework: string[];
+      markup: string[];
+      database: string[];
+      tool: string[];
+      devops: string[];
     };
-    public study: {
-        data: { title: string[]; description: string[]; imgurl: string[] };
-        method: string;
-        paragraph: string;
-    };
+    method: string;
+  };
+  public study: {
+    data: { title: string[]; description: string[]; imgurl: string[] };
+    method: string;
+    paragraph: string;
+  };
 
-    private constructor(data: InfoData) {
-        this.phoneNumber = data.phoneNumber;
-        this.email = data.email;
-        this.github = data.github;
-        this.stack = data.stack;
-        this.study = data.study;
-    }
-    public static fromJson(json: InfoJson): InfoDataStructure {
-        const data: InfoData = {
-            phoneNumber: json.info.phoneNumber,
-            email: json.info.email,
-            github: json.info.github,
-            stack: json.info.stack,
-            study: json.info.study,
-        };
-        return new InfoDataStructure(data);
-    }
+  public constructor(data: InfoData) {
+    this.phoneNumber = data.phoneNumber;
+    this.email = data.email;
+    this.github = data.github;
+    this.stack = data.stack;
+    this.study = data.study;
+  }
+  public static fromJson(json: InfoJson): InfoDataStructure {
+    return fromJsonByKey<InfoData>(json, "info", InfoDataStructure);
+  }
 }
