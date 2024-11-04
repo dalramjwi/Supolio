1384562a9bebebf6b79f0372dde5f4100709ead6
dalramjwi <dalramjwi@gmail.com>
2024-11-04 22:14:41 +0900

    json key 유틸리티 함수 사용 : maindata

--- src/dataStructure/main/class/mainDataStructure.ts
+++ src/dataStructure/main/class/mainDataStructure.ts
@@ -1,26 +1,20 @@
+import { fromJsonByKey } from "../../json/fromJsonByKey.ts";
 import { MainData } from "../interface/mainData.ts";
 import { MainJson } from "../interface/mainJson.ts";
 
 export class MainDataStructure {
-    // public 필드들은 외부에서 접근 가능
-    public name: string;
-    public paragraph: string;
-    public description: string;
+  // public 필드들은 외부에서 접근 가능
+  public name: string;
+  public paragraph: string;
+  public description: string;
 
-    // private 생성자, 외부에서 직접 인스턴스를 만들 수 없도록 함
-    private constructor(data: MainData) {
-        this.name = data.name;
-        this.paragraph = data.paragraph;
-        this.description = data.description;
-    }
-    // JSON 데이터를 받아 인스턴스를 생성하는 정적 메서드
-    public static fromJson(json: MainJson): MainDataStructure {
-        // 이름, 문단, 설명을 추출한 후 객체로 묶어서 생성자에 전달
-        const data: MainData = {
-            name: json.main.name,
-            paragraph: json.main.paragraph,
-            description: json.main.description,
-        };
-        return new MainDataStructure(data);
-    }
+  public constructor(data: MainData) {
+    this.name = data.name;
+    this.paragraph = data.paragraph;
+    this.description = data.description;
+  }
+  // JSON 데이터를 받아 인스턴스를 생성하는 정적 메서드
+  public static fromJson(json: MainJson): MainDataStructure {
+    return fromJsonByKey<MainData>(json, "main", MainDataStructure);
+  }
 }
