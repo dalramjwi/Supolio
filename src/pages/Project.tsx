c1cf3d1ebd94c955d95c3e053aa2ba62be674af2
dalramjwi <dalramjwi@gmail.com>
2024-11-04 22:17:03 +0900

    사용치 않는 project instance 순환 참조 오류 수정 위해 임시 주석화

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+// import { ProjectInstance } from "../dataStructure/project/instance/projectInstance.ts";
 const Project: React.FC = () => {
-  return <div className="flex flex-col min-h-full bg-gray-100 p-6 ">asdf</div>;
+  return <div className="flex flex-col min-h-full bg-gray-100 p-6 "></div>;
 };
 
 export default Project;
