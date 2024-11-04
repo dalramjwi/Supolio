e1ebc9d125242337034c2e82e276fa28ec9a2d63
dalramjwi <dalramjwi@gmail.com>
2024-11-04 13:30:25 +0900

    파일명 변경: infoInstance

--- src/dataStructure/info/instance/infoInstance.ts
+++ src/dataStructure/info/instance/infoInstance.ts
@@ -1,3 +1,3 @@
-import { InfoDataStructure } from "../class/class.ts";
+import { InfoDataStructure } from "../class/infoDataStructure.ts";
 import dataJson from "../../../data/data.json";
 export const InfoInstance = InfoDataStructure.fromJson(dataJson);
