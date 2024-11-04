7345aa46b448101d8b85aaf68a82a98b94c46f4e
dalramjwi <dalramjwi@gmail.com>
2024-11-04 19:01:50 +0900

    project 데이터 연결

--- src/dataStructure/project/class/projectDataStructure.ts
+++ src/dataStructure/project/class/projectDataStructure.ts
@@ -1,44 +1,46 @@
 import { ProjectData } from "../interface/projectData.ts";
 import { ProjectJson } from "../interface/projectJson.ts";
+import { TeamProjectDataStructure } from "./teamProjectDataStructure.ts";
 
+// 기본 ProjectDataStructure 클래스
 export class ProjectDataStructure {
-    public name: string;
-    public description: string;
-    public introduce: string;
-    public detail: string;
-    public contribute?: string;
-    public img: { description: string; imgurl: string }[];
-    public review: string;
-    public period: string[];
-    public stack: string[];
-    public link: (string | string[])[];
-    public thumbnail: string;
+  public name: string;
+  public description: string;
+  public introduce: string;
+  public detail: string;
+  public img: { description: string; imgurl: string }[];
+  public review: string;
+  public period: string[];
+  public stack: string[];
+  public link: (string | string[])[];
+  public thumbnail: string;
 
-    private constructor(data: ProjectData) {
-        this.name = data.name;
-        this.description = data.description;
-        this.introduce = data.introduce;
-        this.detail = data.detail;
-        this.contribute = data.contribute;
-        this.img = data.img;
-        this.review = data.review;
-        this.period = data.period;
-        this.stack = data.stack;
-        this.link = data.link;
-        this.thumbnail = data.thumbnail;
-    }
+  constructor(data: ProjectData) {
+    this.name = data.name;
+    this.description = data.description;
+    this.introduce = data.introduce;
+    this.detail = data.detail;
+    this.img = data.img;
+    this.review = data.review;
+    this.period = data.period;
+    this.stack = data.stack;
+    this.link = data.link;
+    this.thumbnail = data.thumbnail;
+  }
 
-    public static fromJson(json: ProjectJson): ProjectDataStructure[] {
-        const projectDataStructures: ProjectDataStructure[] = [];
+  public static fromJson(json: ProjectJson): ProjectDataStructure[] {
+    const projectDataStructures: ProjectDataStructure[] = [];
 
-        json.project.single.forEach((singleData) => {
-            projectDataStructures.push(new ProjectDataStructure(singleData));
-        });
+    // Single Project 데이터 추가
+    json.project.single.forEach((singleData) => {
+      projectDataStructures.push(new ProjectDataStructure(singleData));
+    });
 
-        json.project.team.forEach((teamData) => {
-            projectDataStructures.push(new ProjectDataStructure(teamData));
-        });
+    // Team Project 데이터 추가
+    json.project.team.forEach((teamData) => {
+      projectDataStructures.push(new TeamProjectDataStructure(teamData));
+    });
 
-        return projectDataStructures;
-    }
+    return projectDataStructures;
+  }
 }
