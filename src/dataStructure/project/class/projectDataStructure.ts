ad8b4ce4ec76962748423ee054efbc660adf5d0a
dalramjwi <dalramjwi@gmail.com>
2024-11-05 22:08:52 +0900

    순환참조 문제 해결 : json 메서드 삭제

--- src/dataStructure/project/class/projectDataStructure.ts
+++ src/dataStructure/project/class/projectDataStructure.ts
@@ -1,20 +1,17 @@
-import { fromJsonByKey } from "../../json/fromJsonByKey.ts";
-import { ProjectContentJson } from "../interface/projectContentJson.ts";
 import { ProjectData } from "../interface/projectData.ts";
-import { ProjectJson } from "../interface/projectJson.ts";
-import { TeamProjectData } from "../interface/teamProjectData.ts";
-import { TeamProjectDataStructure } from "./teamProjectDataStructure.ts";
+// import { ProjectContentJson } from "../interface/projectContentJson.ts";
+// import { TeamProjectData } from "../interface/teamProjectData.ts";
 
 // project 데이터를 담을 수 있는 ProjectContent 클래스
-class ProjectContent {
-  public single: ProjectData[];
-  public team: TeamProjectData[];
+// class ProjectContent {
+//   public single: ProjectData[];
+//   public team: TeamProjectData[];
 
-  constructor(data: ProjectContentJson) {
-    this.single = data.single;
-    this.team = data.team;
-  }
-}
+//   constructor(data: ProjectContentJson) {
+//     this.single = data.single;
+//     this.team = data.team;
+//   }
+// }
 
 // 기본 ProjectDataStructure 클래스
 export class ProjectDataStructure {
@@ -40,20 +37,5 @@ export class ProjectDataStructure {
     this.stack = data.stack;
     this.link = data.link;
     this.thumbnail = data.thumbnail;
-  }
-
-  public static fromJson(json: ProjectJson): ProjectDataStructure[] {
-    // 'project' 키의 데이터를 추출하여 ProjectContent 타입을 사용
-    const projectData = fromJsonByKey(json, "project", ProjectContent);
-
-    const singleProjects = projectData.single.map(
-      (singleData) => new ProjectDataStructure(singleData)
-    );
-
-    const teamProjects = projectData.team.map(
-      (teamData) => new TeamProjectDataStructure(teamData)
-    );
-
-    return [...singleProjects, ...teamProjects];
   }
 }
