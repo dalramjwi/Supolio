4b5851dc3388e6fa59a6e9be3821641afc2e4d89
dalramjwi <dalramjwi@gmail.com>
2024-11-05 22:08:27 +0900

    순환참조 문제 해결 : teamproject의 상속 속성 삭제

--- src/dataStructure/project/class/teamProjectDataStructure.ts
+++ src/dataStructure/project/class/teamProjectDataStructure.ts
@@ -1,11 +1,21 @@
 import { TeamProjectData } from "../interface/teamProjectData.ts";
 import { ProjectDataStructure } from "./projectDataStructure.ts";
 
-export class TeamProjectDataStructure extends ProjectDataStructure {
+// export class TeamProjectDataStructure extends ProjectDataStructure {
+//   public contribute: string;
+
+//   public constructor(data: TeamProjectData) {
+//     super(data);
+//     this.contribute = data.contribute!;
+//   }
+// }
+
+export class TeamProjectDataStructure {
+  public projectData: ProjectDataStructure;
   public contribute: string;
 
-  public constructor(data: TeamProjectData) {
-    super(data);
-    this.contribute = data.contribute!;
+  constructor(data: TeamProjectData) {
+    this.projectData = new ProjectDataStructure(data);
+    this.contribute = data.contribute;
   }
 }
