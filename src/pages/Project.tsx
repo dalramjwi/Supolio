ab4c15e7f974a18886d74acf206fff7f1560baf7
dalramjwi <dalramjwi@gmail.com>
2024-11-05 22:21:30 +0900

    페이지에서 instance 값 랜더링 확인

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-// import { ProjectInstance } from "../dataStructure/project/instance/projectInstance.ts";
-const Project: React.FC = () => {
-  return <div className="flex flex-col min-h-full bg-gray-100 p-6 "></div>;
-};
+import { ProjectInstance } from "../dataStructure/project/instance/projectInstance.ts";
 
+const Project: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-full bg-gray-100 p-6">
+      {ProjectInstance.map((project, index) => (
+        <div key={index} className="mb-4">
+          <h2 className="text-lg font-semibold">{project.name}</h2>
+          <p className="text-sm">{project.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Project;
