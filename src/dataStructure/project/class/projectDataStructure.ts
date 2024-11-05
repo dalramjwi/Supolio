384d4c4ecfd9c9ad7c028f93d3aae66806ee3bc7
dalramjwi <dalramjwi@gmail.com>
2024-11-05 22:20:54 +0900

    fromjson 메서드 활용 : 순환참조 문제 발생하지 않음 체크

--- src/dataStructure/project/class/projectDataStructure.ts
+++ src/dataStructure/project/class/projectDataStructure.ts
@@ -1,4 +1,5 @@
 import { ProjectData } from "../interface/projectData.ts";
+import { ProjectJson } from "../interface/projectJson.ts";
 // import { ProjectContentJson } from "../interface/projectContentJson.ts";
 // import { TeamProjectData } from "../interface/teamProjectData.ts";
 
@@ -19,6 +20,7 @@ export class ProjectDataStructure {
   public description: string;
   public introduce: string;
   public detail: string;
+  public contribute?: string;
   public img: { description: string; imgurl: string }[];
   public review: string;
   public period: string[];
@@ -31,11 +33,25 @@ export class ProjectDataStructure {
     this.description = data.description;
     this.introduce = data.introduce;
     this.detail = data.detail;
+    this.contribute = data.contribute;
     this.img = data.img;
     this.review = data.review;
     this.period = data.period;
     this.stack = data.stack;
     this.link = data.link;
     this.thumbnail = data.thumbnail;
+  }
+  public static fromJson(json: ProjectJson): ProjectDataStructure[] {
+    const projectDataStructures: ProjectDataStructure[] = [];
+
+    json.project.single.forEach((singleData) => {
+      projectDataStructures.push(new ProjectDataStructure(singleData));
+    });
+
+    json.project.team.forEach((teamData) => {
+      projectDataStructures.push(new ProjectDataStructure(teamData));
+    });
+
+    return projectDataStructures;
   }
 }
