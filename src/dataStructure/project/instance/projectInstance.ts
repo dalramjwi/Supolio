197894165da7956ce5c63588d65e997b9d5804d8
dalramjwi <dalramjwi@gmail.com>
2024-11-05 22:21:01 +0900

    fromjson 메서드 활용 : 순환참조 문제 발생하지 않음 체크

--- src/dataStructure/project/instance/projectInstance.ts
+++ src/dataStructure/project/instance/projectInstance.ts
@@ -1,16 +1,3 @@
 import dataJson from "../../../data/data.json";
 import { ProjectDataStructure } from "../class/projectDataStructure.ts";
-import { TeamProjectDataStructure } from "../class/teamProjectDataStructure.ts";
-import { ProjectContentJson } from "../interface/projectContentJson.ts";
-
-// JSON 데이터에서 `single` 및 `team` 데이터 추출 후 각각의 인스턴스 생성
-const projectData = dataJson.project as ProjectContentJson;
-
-const singleProjects = projectData.single.map(
-  (data) => new ProjectDataStructure(data)
-);
-const teamProjects = projectData.team.map(
-  (data) => new TeamProjectDataStructure(data)
-);
-
-export const ProjectInstance = [...singleProjects, ...teamProjects];
+export const ProjectInstance = ProjectDataStructure.fromJson(dataJson);
