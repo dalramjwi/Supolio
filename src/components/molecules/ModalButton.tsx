980fbf34b71febef5ed8d7e1d9e93a1df7261fec
dalramjwi <dalramjwi@gmail.com>
2024-11-07 22:54:57 +0900

    경로 수정

--- src/components/molecules/ModalButton.tsx
+++ src/components/molecules/ModalButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ModalButtonProps } from "../interfaces/organisms/ModalButton.interface";
-import Button from "../atoms/Button";
+import Button from "../atoms/Button.tsx";
 //todo css 처리 필요
 const ModalButton: React.FC<ModalButtonProps> = ({
   direction,
