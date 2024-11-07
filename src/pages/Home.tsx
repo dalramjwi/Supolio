9d08ad4cacd4cf6784ae15d2467195128e07d6c8
dalramjwi <dalramjwi@gmail.com>
2024-11-07 22:42:36 +0900

    임시 home 생성

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Link to="/main">asd</Link>
+      <Link to="/info">asd</Link>
+      <Link to="/project"> asd</Link>
+    </>
+  );
+};
+
+export default Home;
