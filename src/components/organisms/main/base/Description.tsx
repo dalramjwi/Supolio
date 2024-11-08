b2a3f1352334d115964fc5a8a4db9aa0862c993e
dalramjwi <dalramjwi@gmail.com>
2024-11-08 16:11:50 +0900

    경로 수정

---
+++ src/components/organisms/main/base/Description.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HtmlElementDiv from "./../../../molecules/base/HtmlElementDiv.tsx";
+import { MainInstance } from "../../../../dataStructure/main/instance/mainInstance.ts";
+//todo css 빼기
+const DescriptionSection: React.FC = () => {
+  return (
+    <HtmlElementDiv
+      className="text-xl text-gray-600 mb-4 mt-3 relative z-10"
+      htmlContent={MainInstance.description}
+    ></HtmlElementDiv>
+  );
+};
+
+export default DescriptionSection;
