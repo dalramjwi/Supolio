f67ad5fdf2913777492589c301b5dc5527a6b554
dalramjwi <dalramjwi@gmail.com>
2024-11-09 00:45:24 +0900

    검색 컴포넌트 추가

---
+++ src/components/molecules/css/SearchBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as styles from "./SearchBar.css.ts";
+
+const SearchBar: React.FC = () => {
+  return (
+    <div className={styles.searchContainer}>
+      <div className={styles.placeholderText}>
+        김수현의 포트폴리오
+        <span className={styles.blinkingCursor}>|</span>
+      </div>
+      <div className={styles.searchIcon}></div>
+    </div>
+  );
+};
+
+export default SearchBar;
