86c1e8e425aee31ec4892cb1cf6ff666e1bfdd67
dalramjwi <dalramjwi@gmail.com>
2024-11-08 23:25:47 +0900

    스타일링 된 컴포넌트 제작

---
+++ src/components/molecules/styled/SHomeNavLink.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CustomNavLink from "../../atoms/base/NavLink.tsx";
+import * as styles from "../css/HomeNavLink.css.ts";
+import Dog from "../css/dog.tsx";
+import Div from "../../atoms/base/Div.tsx";
+//! 추후에 로직 변경 고려
+const SHomeNavLink: React.FC = () => {
+  return (
+    <Div className={styles.container}>
+      <Dog />
+      <Div className={styles.linkContainer}>
+        <CustomNavLink to="/main" className={styles.linkStyle}>
+          Main
+        </CustomNavLink>
+        <CustomNavLink to="/info" className={styles.linkStyle}>
+          Info
+        </CustomNavLink>
+        <CustomNavLink to="/project" className={styles.linkStyle}>
+          Projects
+        </CustomNavLink>
+      </Div>
+    </Div>
+  );
+};
+
+export default SHomeNavLink;
