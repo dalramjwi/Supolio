ef2f35a9db3ae8d507a9afc206c6c4ab7c11650f
dalramjwi <dalramjwi@gmail.com>
2024-11-09 00:45:31 +0900

    검색 컴포넌트 연결

--- src/components/molecules/styled/SHomeNavLink.tsx
+++ src/components/molecules/styled/SHomeNavLink.tsx
@@ -3,11 +3,13 @@ import CustomNavLink from "../../atoms/base/NavLink.tsx";
 import * as styles from "../css/HomeNavLink.css.ts";
 import Dog from "../css/dog.tsx";
 import Div from "../../atoms/base/Div.tsx";
+import SearchBar from "../css/SearchBar.tsx";
 //! 추후에 로직 변경 고려
 const SHomeNavLink: React.FC = () => {
   return (
     <Div className={styles.container}>
       <Dog />
+      <SearchBar />
       <Div className={styles.linkContainer}>
         <CustomNavLink to="/main" className={styles.linkStyle}>
           Main
