3db2a5568109e8f1a9fc02474a17b59bf178e885
dalramjwi <dalramjwi@gmail.com>
2024-11-08 15:36:05 +0900

    vanilia extract test

--- src/components/molecules/HomeNavLink.tsx
+++ src/components/molecules/HomeNavLink.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CustomNavLink from "../atoms/NavLink.tsx";
+import { container } from "./app.css.ts";
 //! 추후에 path json으로 data 로직화 하는 것 고려
 const HomeNavLink: React.FC = () => {
   return (
@@ -7,6 +8,7 @@ const HomeNavLink: React.FC = () => {
       <CustomNavLink to="/main">Main</CustomNavLink>
       <CustomNavLink to="/info">Info</CustomNavLink>
       <CustomNavLink to="/project">Project</CustomNavLink>
+      <div className={container}>Hello, Vanilla Extract!</div>
     </>
   );
 };
---
+++ src/components/molecules/app.css.ts
@@ -0,0 +1,8 @@
+// App.css.ts
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  padding: "16px",
+  backgroundColor: "lightblue",
+  fontSize: "1.2rem",
+});
