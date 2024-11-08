92579fafe6363f2ed1c89297e170556504d5d192
dalramjwi <dalramjwi@gmail.com>
2024-11-09 00:45:01 +0900

    세부 스타일 조정

--- src/components/molecules/css/dog.css.ts
+++ src/components/molecules/css/dog.css.ts
@@ -54,8 +54,8 @@ const dogRead = keyframes({
 // 스타일 정의
 export const dogContainer = style({
   position: "fixed", // 고정 위치
-  bottom: "2vh", // 화면의 하단에서 약간 띄움 (vh 단위 사용)
-  left: "5vw", // 화면의 왼쪽에서 약간 띄움 (vw 단위 사용)
+  bottom: "5vh", // 화면의 하단에서 약간 띄움 (vh 단위 사용)
+  left: "6.5vw", // 화면의 왼쪽에서 약간 띄움 (vw 단위 사용)
   height: "18vh", // 화면 높이에 비례한 크기 설정
   zIndex: 10, // 레이어를 최상단에 위치시킴
 
