bed3b525dd7073c97b208618ca460a78590c83a8
dalramjwi <dalramjwi@gmail.com>
2024-11-10 00:40:44 +0900

    styled content 추가

---
+++ src/components/templates/main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SMainContent from "../../organisms/main/styled/SMainContent.tsx";
+
+const Main: React.FC = () => {
+  return (
+    <>
+      <SMainContent />
+    </>
+  );
+};
+
+export default Main;
