f7d7ef20924bf8a632d8e274de2681eee95b3717
dalramjwi <dalramjwi@gmail.com>
2024-11-10 00:39:50 +0900

    maincontent styled 컴포넌트 추가

---
+++ src/components/organisms/main/styled/SMainContent.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import NameSection from "../../../molecules/main/base/NameSection.tsx";
+import ParagraphSection from "../../../molecules/main/base/ParagraphSection.tsx";
+import DescriptionSection from "../../../molecules/main/base/Description.tsx";
+import Div from "../../../atoms/base/Div.tsx";
+import * as styles from "../css/MainContent.css.ts";
+
+const SMainContent: React.FC = () => {
+  return (
+    <Div className={styles.pageContainer}>
+      <Div className={styles.card} tabIndex={0}>
+        <Div className="row clearfix">
+          <Div className="left big">
+            <NameSection />
+          </Div>
+          <Div className="right">
+            <ParagraphSection />
+          </Div>
+        </Div>
+        <Div className="row">
+          <DescriptionSection />
+        </Div>
+      </Div>
+    </Div>
+  );
+};
+
+export default SMainContent;
