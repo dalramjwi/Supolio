bd7969425c00c16e208b9724b98a047a3690112e
dalramjwi <dalramjwi@gmail.com>
2024-11-10 00:40:28 +0900

    경로변경

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SHomeNavLink from "../../molecules/styled/SHomeNavLink.tsx";
+import SHomeNavLink from "../../molecules/home/styled/SHomeNavLink.tsx";
 
 const Home: React.FC = () => {
   return (
