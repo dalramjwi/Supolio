ca274fcf3977822f13f002904b748ad5a4f5606f
dalramjwi <dalramjwi@gmail.com>
2024-11-10 13:04:23 +0900

    desk styling을 위해 임시 주석 처리

--- src/components/organisms/main/styled/SMainContent.tsx
+++ src/components/organisms/main/styled/SMainContent.tsx
@@ -4,24 +4,27 @@ import ParagraphSection from "../../../molecules/main/base/ParagraphSection.tsx"
 import DescriptionSection from "../../../molecules/main/base/Description.tsx";
 import Div from "../../../atoms/base/Div.tsx";
 import * as styles from "../css/MainContent.css.ts";
+import Desk from "../../../appearance/views/Desk.tsx";
 
 const SMainContent: React.FC = () => {
   return (
-    <Div className={styles.pageContainer}>
-      <Div className={styles.card} tabIndex={0}>
-        <Div className="row clearfix">
-          <Div className="left big">
-            <NameSection />
+    <Desk>
+      {/* <Div className={styles.pageContainer}>
+        <Div className={styles.card} tabIndex={0}>
+          <Div className="row clearfix">
+            <Div className="left big">
+              <NameSection />
+            </Div>
+            <Div className="right">
+              <ParagraphSection />
+            </Div>
           </Div>
-          <Div className="right">
-            <ParagraphSection />
+          <Div className="row">
+            <DescriptionSection />
           </Div>
         </Div>
-        <Div className="row">
-          <DescriptionSection />
-        </Div>
-      </Div>
-    </Div>
+      </Div> */}
+    </Desk>
   );
 };
 
