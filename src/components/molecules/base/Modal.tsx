3a3a883207bfc7f9749aa3e96a788f0ea2a53c1d
dalramjwi <dalramjwi@gmail.com>
2024-11-12 00:07:30 +0900

    modal zindex 1000, 최상으로 처리

--- src/components/molecules/base/Modal.tsx
+++ src/components/molecules/base/Modal.tsx
@@ -7,7 +7,7 @@ const Modal: React.FC<ModalProps> = ({ show, close, content }) => {
 
   return (
     <Div
-      className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
+      className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-1000"
       onClick={close} // 모달 외부를 클릭하면 닫힘
     >
       <Div
