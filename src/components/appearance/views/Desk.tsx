80dad9878e68692f713ff5f8780150afce7008f2
dalramjwi <dalramjwi@gmail.com>
2024-11-11 14:22:05 +0900

    dog main 용 파일 제작 및 파일 명 대소문자 구분

--- src/components/appearance/views/Desk.tsx
+++ src/components/appearance/views/Desk.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import * as styles from "../css/desk.css.ts";
+import * as styles from "../css/Desk.css.ts";
 
 interface DeskProps {
   children: JSX.Element | JSX.Element[];
--- src/components/organisms/main/styled/SMainContent.tsx
+++ src/components/organisms/main/styled/SMainContent.tsx
@@ -5,6 +5,7 @@ import Div from "../../../atoms/base/Div.tsx";
 import * as styles from "../css/MainContent.css.ts";
 import Desk from "../../../appearance/views/Desk.tsx";
 import Modal from "../../../molecules/base/Modal.tsx";
+import Dog from "../../../appearance/views/Dog.tsx";
 
 const SMainContent: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,32 +19,35 @@ const SMainContent: React.FC = () => {
   };
 
   return (
-    <Desk>
-      <Div className={styles.pageContainer}>
-        {/* 클릭 가능한 명함 */}
-        <Div className={styles.card} onClick={handleCardClick}>
-          <Div className={styles.company}>
-            <p className={styles.contactTitle}>
-              <NameSection />
-            </p>
-            <p className={styles.position}>Cogito, ergo sum</p>
-            <p className={styles.contactInfo}>010 9823 3662</p>
-            <p className={styles.contactInfo}>dalramjwi@gmail.com</p>
-          </Div>
-        </Div>
-
-        {/* 모달 창 - 명함의 자세한 내용을 표시 */}
-        <Modal
-          show={isModalOpen}
-          close={closeModal}
-          content={
-            <Div className={styles.modalContent}>
-              <DescriptionSection />
+    <>
+      <Dog />
+      <Desk>
+        <Div className={styles.pageContainer}>
+          {/* 클릭 가능한 명함 */}
+          <Div className={styles.card} onClick={handleCardClick}>
+            <Div className={styles.company}>
+              <p className={styles.contactTitle}>
+                <NameSection />
+              </p>
+              <p className={styles.position}>Cogito, ergo sum</p>
+              <p className={styles.contactInfo}>010 9823 3662</p>
+              <p className={styles.contactInfo}>dalramjwi@gmail.com</p>
             </Div>
-          }
-        />
-      </Div>
-    </Desk>
+          </Div>
+
+          {/* 모달 창 - 명함의 자세한 내용을 표시 */}
+          <Modal
+            show={isModalOpen}
+            close={closeModal}
+            content={
+              <Div className={styles.modalContent}>
+                <DescriptionSection />
+              </Div>
+            }
+          />
+        </Div>
+      </Desk>
+    </>
   );
 };
 
