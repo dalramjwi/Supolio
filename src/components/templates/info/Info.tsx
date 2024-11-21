493c40ba1db8e48e766e092285f8555c261bc071
dalramjwi <dalramjwi@gmail.com>
2024-11-21 22:09:21 +0900

    infolaptop을 연결한 info.tsx *임시

--- src/components/templates/info/Info.tsx
+++ src/components/templates/info/Info.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import InfoContent from "../../organisms/info/base/InfoContent.tsx";
+import InfoLaptop from "../../appearance/views/InfoLaptop.tsx";
 
 const Info: React.FC = () => {
-  return <>{/* <InfoContent /> */}</>;
+  return (
+    <>
+      {/* <InfoContent /> */}
+      <InfoLaptop />
+    </>
+  );
 };
 
 export default Info;
